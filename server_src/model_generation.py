08b5cf4b26b24d256e01821cbe231b772b2ac943
luxolisHcanon <hugo.canon@luxpmsoft.com>
2023-11-29 15:07:47 +0900

    feat: finished

--- server_src/model_generation.py
+++ server_src/model_generation.py
@@ -49,7 +49,7 @@ def generate_model_from_nerf_model(nerf_model_path):
 
 async def generate_and_upload_3d_model(s3_path):
     video_path, s3_folder = download_object_from_s3(s3_path)
-    # photos_path = split_video_into_pictures(video_path)
-    # nerf_model_path = generate_nerf_model_from_photo_set(photos_path)
-    # model_output_folder = generate_model_from_nerf_model(nerf_model_path)
-    upload_full_directory_to_s3("./output", s3_folder)
+    photos_path = split_video_into_pictures(video_path)
+    nerf_model_path = generate_nerf_model_from_photo_set(photos_path)
+    model_output_folder = generate_model_from_nerf_model(nerf_model_path)
+    upload_full_directory_to_s3(model_output_folder, s3_folder)
