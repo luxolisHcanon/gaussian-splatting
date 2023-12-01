b90cf8f21e0fe4d2c1fb8123a0c2fbc4c73ffb8d
luxolisHcanon <hugo.canon@luxpmsoft.com>
2023-12-01 10:23:23 +0900

    feat: removed zip extracting

--- server_src/s3_utils.py
+++ server_src/s3_utils.py
@@ -2,7 +2,6 @@ from . aws import get_s3_session
 import os
 from botocore.exceptions import NoCredentialsError
 import uuid
-import zipfile
 
 
 def generate_unique_folder(base_path):
@@ -14,43 +13,25 @@ def generate_unique_folder(base_path):
         folder_name = str(uuid.uuid4())
 
     os.mkdir(base_path + folder_name)
-    os.mkdir(base_path + folder_name + "/zip_file")
     return folder_name
 
 
-def unzip_file(file_path, destination_path):
-    # Unzip
-    with zipfile.ZipFile(file_path, "r") as zip_ref:
-        zip_ref.extractall(destination_path)
-
-    # Get file name
-    folder_content = os.listdir(destination_path)
-    files = [item for item in folder_content if os.path.isfile(os.path.join(destination_path, item))]
-    if len(files) == 1:
-        return os.path.join(destination_path, files[0])
-    else:
-        raise Exception("Zip file does not contain exactly one file.")
-
-
-def download_and_unzip_file_from_s3(file_path):
+def download_file_from_s3(s3_file_path):
     session = get_s3_session()
 
-    split_file = file_path.split("/")
+    split_file = s3_file_path.split("/")
     length_split = len(split_file) - 1
     file_name = split_file[length_split]
     split_file.pop(length_split)
     s3_folder = "/".join(split_file)
 
     base_path = "/tmp/gaussian-splatting/"
-    workdir = base_path + generate_unique_folder(base_path)
-    file_zip_path = workdir + "/zip_file/" + file_name
+    downloaded_file_path = base_path + generate_unique_folder(base_path) + file_name
 
-    s3_client = session.s3_client
-    s3_client.download_file(session.bucket_name, file_path, file_zip_path)
+    session.s3_client.download_file(session.bucket_name, s3_file_path, downloaded_file_path)
 
-    extracted_file_path = unzip_file(file_zip_path, workdir)
-    print("file_path:", extracted_file_path)
-    return extracted_file_path, s3_folder
+    print("file_path:", downloaded_file_path)
+    return downloaded_file_path, s3_folder
 
 
 def upload_directory_to_s3(path, destination, session):
