91d1a6d5d8e4ae8017ff5d3d56ab07de4d9ac58b
Hcanon <hugo.canon@luxpmsoft.com>
2023-12-22 15:33:38 +0900

    fix: add create input folder function

--- server_src/file_utils.py
+++ server_src/file_utils.py
@@ -1,5 +1,6 @@
 import zipfile
 import shutil
+import os
 
 
 def get_folder_from_path(file_path):
@@ -8,13 +9,17 @@ def get_folder_from_path(file_path):
     return "/".join(split_file)
 
 
-def unzip_folder(zip_path):
-    print('zip_path =', zip_path)
-    folder_path = get_folder_from_path(zip_path)
-    print('folder_path =', folder_path)
+def unzip_folder(zip_path, destination_path):
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-        zip_ref.extractall(folder_path)
-    return folder_path
+        zip_ref.extractall(destination_path)
+
+
+def create_input_folder(file_path):
+    workdir_path = get_folder_from_path(file_path)
+    input_folder_path = os.path.join(workdir_path, "input")
+    if not os.path.exists(input_folder_path):
+        os.makedirs(input_folder_path)
+    return workdir_path
 
 
 def clean_files(path):
--- server_src/model_generation.py
+++ server_src/model_generation.py
@@ -4,43 +4,36 @@ import shutil
 import subprocess
 
 from . s3_utils import download_file_from_s3, upload_full_directory_to_s3
-from . file_utils import get_folder_from_path, unzip_folder, clean_files
+from . file_utils import create_input_folder, unzip_folder, clean_files
 
 
-def split_video_into_pictures(video_path):
+def split_video_into_pictures(video_path, destination_path):
     # Split Video into pictures
     subprocess.run(["ffmpeg", "-i", video_path, "-qscale:v", "1", "-qmin", "1", "-vf", "fps=2", "%04d.jpg"])
 
-    return get_folder_from_path(video_path)
-
-
-def generate_nerf_model_from_photo_set(photos_path):
     # Create input folder and move all photos in it
-    input_folder_path = os.path.join(photos_path, "input")
-    if not os.path.exists(input_folder_path):
-        os.makedirs(input_folder_path)
     for photo in glob.glob('./*.jpg'):
-        shutil.move(photo, input_folder_path)
+        shutil.move(photo, destination_path)
 
+
+def generate_nerf_model_from_photo_set(workdir_path):
     # Process the photos
-    subprocess.run(["python", "./gaussian_splatting_src/convert.py", "-s", photos_path, "--no_gpu"])
-
-    return photos_path
+    subprocess.run(["python", "./gaussian_splatting_src/convert.py", "-s", workdir_path, "--no_gpu"])
 
 
-def generate_model_from_nerf_model(nerf_model_path):
+def generate_model_from_nerf_model(workdir_path):
     # Train the model
-    subprocess.run(["python", "./gaussian_splatting_src/train.py", "-s", nerf_model_path])
+    subprocess.run(["python", "./gaussian_splatting_src/train.py", "-s", workdir_path])
 
     # Move output folder to work directory
-    shutil.move("./output", nerf_model_path)
-    output_folder_path = os.path.join(nerf_model_path, "output")
+    shutil.move("./output", workdir_path)
+    output_folder_path = os.path.join(workdir_path, "output")
     return output_folder_path
 
 
-def generate_and_upload_3d_model(photos_path, s3_folder):
-    nerf_model_path = generate_nerf_model_from_photo_set(photos_path)
-    model_output_folder = generate_model_from_nerf_model(nerf_model_path)
+def generate_and_upload_3d_model(workdir_path, s3_folder):
+    generate_nerf_model_from_photo_set(workdir_path)
+    model_output_folder = generate_model_from_nerf_model(workdir_path)
     upload_full_directory_to_s3(model_output_folder, s3_folder)
     # clean_files(nerf_model_path)
 
@@ -48,8 +41,9 @@ def generate_and_upload_3d_model(photos_path, s3_folder):
 async def generate_and_upload_3d_model_from_video(s3_path):
     try:
         video_path, s3_folder = download_file_from_s3(s3_path)
-        photos_path = split_video_into_pictures(video_path)
-        generate_and_upload_3d_model(photos_path, s3_folder)
+        workdir_path, input_folder_path = create_input_folder(video_path)
+        split_video_into_pictures(video_path, input_folder_path)
+        generate_and_upload_3d_model(workdir_path, s3_folder)
     except Exception as e:
         print(f"Exception raised: {e}")
         return 500
@@ -59,8 +53,9 @@ async def generate_and_upload_3d_model_from_video(s3_path):
 async def generate_and_upload_3d_model_from_photos(s3_path):
     try:
         zip_path, s3_folder = download_file_from_s3(s3_path)
-        photos_path = unzip_folder(zip_path)
-        generate_and_upload_3d_model(photos_path, s3_folder)
+        workdir_path, input_folder_path = create_input_folder(zip_path)
+        unzip_folder(zip_path, input_folder_path)
+        generate_and_upload_3d_model(workdir_path, s3_folder)
     except Exception as e:
         print(f"Exception raised: {e}")
         return 500
