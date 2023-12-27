77cd48c7cf0e4f23e052f16ed8297e6e8e775c0d
Hcanon <hugo.canon@luxpmsoft.com>
2023-12-27 17:03:36 +0900

    fix: give json to request

--- server_src/services/api.py
+++ server_src/services/api.py
@@ -1,9 +1,10 @@
 import requests
+import json
 
 
 def post_request(url, payload, x_access_token=None):
     headers = {}
     if x_access_token is not None:
         headers = {"x-access-token": x_access_token}
-    result = requests.post(url, headers=headers, data=payload)
+    result = requests.post(url, headers=headers, json=json.dumps(payload))
     return result
