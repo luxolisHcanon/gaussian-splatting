8f87524d03a6d90f5a576c944d80075a65a867ea
Hcanon <hugo.canon@luxpmsoft.com>
2023-12-27 17:25:50 +0900

    fix: test

--- server_src/services/api.py
+++ server_src/services/api.py
@@ -6,5 +6,5 @@ def post_request(url, payload, x_access_token=None):
     headers = {}
     if x_access_token is not None:
         headers = {"x-access-token": x_access_token}
-    result = requests.post(url, headers=headers, json=json.dumps(payload))
+    result = requests.post(url, headers=headers, data=json.dumps(payload))
     return result
